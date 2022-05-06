a1a0d034ad8483dce64299f5d202ba9a46e960fd
Parth Shah <shah.parth3@northeastern.edu>
2022-05-06 03:48:26 -0400

    added

--- Pages/Only_News.py
+++ Pages/Only_News.py
@@ -5,7 +5,8 @@ from helper import change,keyword_beautificaiton,get_image,clear_summary
 from PIL import Image
 import json
 import email_html
-import NER
+
+# import NER
 from datetime import datetime
 now = datetime.now()
  
@@ -13,13 +14,12 @@ now = datetime.now()
 
 
 def news_main(country_selections,topic_selections,username):
-    a = st.text_input("Ask us a Question")
-    list_keyword = NER.NER_keywrod(a)
-    a = st.text_input("Ask us a Question")
-    if len(list_keyword)> 0:
-        all_data= Get_data.regex_filter_data(list_keyword)
-    else:
-        all_data = Get_data.filter_data(country_selections,topic_selections)
+    # a = st.text_input("Ask us a Question")
+    # list_keyword = NER.NER_keywrod(a)
+    # if len(a)> 0:
+    #     all_data= Get_data.regex_filter_data(list_keyword)
+    # else:
+    all_data = Get_data.filter_data(country_selections,topic_selections)
     def news_beautification(column,news_title,news_sentiment_score,news_sentiment_text,keywords,save_path,summary,country,source):
         image = Image.open(save_path)
         column.image(image,width=360)
